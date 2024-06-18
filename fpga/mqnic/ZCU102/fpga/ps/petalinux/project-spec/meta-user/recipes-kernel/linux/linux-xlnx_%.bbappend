46b0b427f38ab01e03b5efd4a42eb219a40e7232
Jiefan Zhang <zjiefan@gmail.com>
2024-06-18 00:05:41 -0400

    remove fpga/mqnic/ other boards

--- fpga/mqnic/ZCU102/fpga/ps/petalinux/project-spec/meta-user/recipes-kernel/linux/linux-xlnx_%.bbappend
+++
@@ -1,6 +0,0 @@
-SRC_URI += "file://enable_ptp.cfg\
-            file://enable_dsa.cfg\
-            "
-
-FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
-
--- fpga/mqnic/ZCU106/fpga_zynqmp/ps/petalinux/local.mk
+++
@@ -1,8 +0,0 @@
-HDF ?= ../../fpga/fpga.xsa
-
-# shortcut to build PetaLinux project including boot files
-build-boot:
-	$(MAKE) build
-	$(MAKE) package-boot
-
-.PHONY: build-boot
